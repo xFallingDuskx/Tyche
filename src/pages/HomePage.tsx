0144054ffd643e0713c45de4cc22442915fd2d3e
Stephon Ricks <stephon.ricks.01@gmail.com>
2023-09-19 21:02:45 -0400

    auth: handle sign out and redirect
    - allow users to sign out from home page
    - prevent users that are not logged in from accessing page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,36 @@
 import { useAuth } from '../contexts/AuthContext'
+import signOutUser from '../firebase/auth/signOut'
+
 
 const HomePage = () => {
     const { currentUser } = useAuth()
+    if (! currentUser) {
+        document.location = '/'
+        return
+    }
+
+    const handleSignOut = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        const promise = signOutUser()
+        promise
+            .then(response => {
+                if (! response.error) {
+                    document.location = '/home'
+                }
+            })
+    }
+
+    const content = (
+        <div id='main-container' className='h-screen w-full flex flex-col'>
+            <button onClick={handleSignOut} className='self-start mt-3 p-1 animated-btn-1 bg-red-300'>Sign Out</button>
+            <p>Your email is {currentUser.email}</p>
+        </div>
+    )
+
     return (
-        <p>Your email is {currentUser?.email}</p>
+        <>
+            {currentUser ? content : null}
+        </> 
     )
 }
 
